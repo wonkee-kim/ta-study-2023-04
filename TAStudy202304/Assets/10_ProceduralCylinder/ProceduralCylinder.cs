6ec3a469c47fe6c70d75f31dd168f1f2f7b99f0d
Wonkee Kim <kimwonky@gmail.com>
2024-09-15 10:42:35 -0400

    w

--- TAStudy202304/Assets/10_ProceduralCylinder/ProceduralCylinder.cs
+++ TAStudy202304/Assets/10_ProceduralCylinder/ProceduralCylinder.cs
@@ -4,6 +4,20 @@ using UnityEngine.Rendering;
 // public class ProceduralCylinder : MonoBehaviour, System.IDisposable
 public class ProceduralCylinder : MonoBehaviour
 {
+    private const string KERNEL_NAME_VERTEX_BUFFER_UPDATE = "VertexBufferUpdate";
+    private const string KERNEL_NAME_INDEX_BUFFER_UPDATE = "IndexBufferUpdate";
+    private int _kernelVertexBufferUpdate;
+    private int _kernelIndexBufferUpdate;
+
+    private static readonly int PROP_RADIAL_RESOLUTION = Shader.PropertyToID("_RadialResolution");
+    private static readonly int PROP_HEIGHT_RESOLUTION = Shader.PropertyToID("_HeightResolution");
+    private static readonly int PROP_RADIUS = Shader.PropertyToID("_Radius");
+    private static readonly int PROP_HEIGHT = Shader.PropertyToID("_Height");
+    private static readonly int PROP_VERTEX_COUNT = Shader.PropertyToID("_VertexCount");
+    private static readonly int PROP_QUAD_COUNT = Shader.PropertyToID("_QuadCount");
+    private static readonly int PROP_VERTICES = Shader.PropertyToID("_Vertices");
+    private static readonly int PROP_INDICES = Shader.PropertyToID("_Indices");
+
     [Header("Mesh Settings")]
     [SerializeField] private int radialResolution = 32;
     [SerializeField] private int heightResolution = 16;
@@ -17,6 +31,15 @@ public class ProceduralCylinder : MonoBehaviour
     private Mesh _mesh;
     private GraphicsBuffer _vertexBuffer;
     private GraphicsBuffer _indexBuffer;
+    private int _vertexCount;
+    private int _quadCount;
+
+
+    private void Awake()
+    {
+        _kernelVertexBufferUpdate = _compute.FindKernel(KERNEL_NAME_VERTEX_BUFFER_UPDATE);
+        _kernelIndexBufferUpdate = _compute.FindKernel(KERNEL_NAME_INDEX_BUFFER_UPDATE);
+    }
 
     private void Start()
     {
@@ -56,14 +79,9 @@ public class ProceduralCylinder : MonoBehaviour
         _vertexBuffer?.Dispose();
         _indexBuffer?.Dispose();
 
-        int vertexCountSide = (heightResolution + 1) * radialResolution;
-        int vertexCountTopBottom = radialResolution * 2;
-        int vertexCount = vertexCountSide + vertexCountTopBottom;
-
-        int triangleCountSide = heightResolution * radialResolution * 2;
-        int triangleCountTopBottom = radialResolution * 2;
-        int triangleCount = triangleCountSide + triangleCountTopBottom;
-
+        _quadCount = heightResolution * radialResolution;
+        _vertexCount = (heightResolution + 1) * radialResolution;
+        int triangleCount = heightResolution * radialResolution * 2;
         int indexCount = triangleCount * 3;
 
         // Vertex attribute descriptor
@@ -78,11 +96,11 @@ public class ProceduralCylinder : MonoBehaviour
             (VertexAttribute.Normal, VertexAttributeFormat.Float32, 3);
 
         // Vertex/index buffer formats
-        _mesh.SetVertexBufferParams(vertexCount, vp, vn);
+        _mesh.SetVertexBufferParams(_vertexCount, vp, vn);
         _mesh.SetIndexBufferParams(indexCount, IndexFormat.UInt32);
 
         // Submesh initialization
-        _mesh.SetSubMesh(0, new SubMeshDescriptor(0, vertexCount), MeshUpdateFlags.DontRecalculateBounds);
+        _mesh.SetSubMesh(0, new SubMeshDescriptor(0, _vertexCount), MeshUpdateFlags.DontRecalculateBounds);
 
         // GraphicsBuffer references
         _vertexBuffer = _mesh.GetVertexBuffer(0);
@@ -91,18 +109,28 @@ public class ProceduralCylinder : MonoBehaviour
 
     private void UpdateMesh()
     {
-        _compute.SetInt("_RadialResolution", radialResolution);
-        _compute.SetInt("_HeightResolution", heightResolution);
-        _compute.SetFloat("_Radius", radius);
-        _compute.SetFloat("_Height", height);
+        _compute.SetInt(PROP_RADIAL_RESOLUTION, radialResolution);
+        _compute.SetInt(PROP_HEIGHT_RESOLUTION, heightResolution);
+        _compute.SetFloat(PROP_RADIUS, radius);
+        _compute.SetFloat(PROP_HEIGHT, height);
+        _compute.SetInt(PROP_VERTEX_COUNT, _vertexCount);
+        _compute.SetInt(PROP_QUAD_COUNT, _quadCount);
 
-        _compute.SetBuffer(0, "_Vertices", _vertexBuffer);
-        DispatchThreads(_compute, 0, _vertexBuffer.count);
+        _compute.SetBuffer(_kernelVertexBufferUpdate, PROP_VERTICES, _vertexBuffer);
+        DispatchThreads(_compute, _kernelVertexBufferUpdate, _vertexCount);
 
-        _compute.SetBuffer(1, "_Indices", _indexBuffer);
-        DispatchThreads(_compute, 1, _indexBuffer.count);
+        // _compute.SetBuffer(_kernelIndexBufferUpdate, PROP_INDICES, _indexBuffer);
+        // DispatchThreads(_compute, _kernelIndexBufferUpdate, _quadCount); // Quad per thread
 
         _mesh.bounds = new Bounds(Vector3.zero, Vector3.one * (1 + radius * 2));
+
+
+        int[] indices = new int[_vertexCount];
+        for (int i = 0; i < indices.Length; i++)
+        {
+            indices[i] = i;
+        }
+        _mesh.SetIndices(indices, MeshTopology.Points, 0);
     }
 
     // Execute a compute shader with specifying a minimum number of thread
